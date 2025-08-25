9e7869b3b740fa7449c210786fafc17f52517746
RuslanTopal <baranotik15@gmail.com>
2025-08-25 03:29:27 +0000

    fix bug

--- task.sql
+++ task.sql
@@ -1,8 +1,8 @@
 # Write your SQL code for the database creation here. Good luck! 
 USE ShopDB;
 
-CREATE INDEX idx_email ON Customers (Email);
-CREATE INDEX idx_email ON Products (Name);
+CREATE INDEX Email ON Customers (Email);
+CREATE INDEX Name ON Products (Name);
 
 SHOW INDEX FROM ShopDB.Customers;
 SHOW INDEX FROM ShopDB.Products;
